9be6dead254b4274ca327669ecb83114d9ff9ceb
Emoji <studyemoji@mail.ru>
2024-10-02 03:52:18 +0300

    updated res/values
    new dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 }
 
 dependencies {
+    runtimeOnly("androidx.compose.material3:material3:1.3.0")
+    runtimeOnly("androidx.compose.ui:ui:1.7.2")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
