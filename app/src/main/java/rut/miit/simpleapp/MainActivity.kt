0a77fed39bd76a58cc73642a03bf3ccf94aa36ad
Emoji <studyemoji@mail.ru>
2024-11-14 04:35:31 +0300

    do not f-ing know what to do with action bar

--- app/src/main/java/rut/miit/simpleapp/MainActivity.kt
+++ app/src/main/java/rut/miit/simpleapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package rut.miit.simpleapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,14 +17,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val toolbar: Toolbar = findViewById(R.id.toolbar)
-//        setSupportActionBar(toolbar)
+        // Находим кастомный тулбар
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        // Устанавливаем тулбар как ActionBar
+        setSupportActionBar(toolbar)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragmentContainer) as NavHostFragment
         navController = navHostFragment.navController
 
-//        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings -> {
+                navController.navigate(R.id.settingsFragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/rut/miit/simpleapp/fragments/SettingsFragment.kt
+++ app/src/main/java/rut/miit/simpleapp/fragments/SettingsFragment.kt
@@ -4,33 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import rut.miit.simpleapp.R
-import rut.miit.simpleapp.databinding.FragmentOnboardBinding
+import rut.miit.simpleapp.databinding.FragmentSettingsBinding
 
-class OnboardFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentOnboardBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding ?: throw IllegalStateException("Binding should not be accessed before onCreateView or after onDestroyView")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentOnboardBinding.inflate(inflater, container, false)
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Показываем тулбар на этом фрагменте
-        (activity as? AppCompatActivity)?.supportActionBar?.show()
-
-        binding.buttonSignIn.setOnClickListener {
-            findNavController().navigate(R.id.action_onboardFragment_to_signInFragment)
-        }
+        binding.textViewSettings.text = "Настройки приложения"
     }
 
     override fun onDestroyView() {
